41f8c7bffd3dc3a0bdd4cc940402b437f5e79396
jameshall1999 <jamesbh1@outlook.com>
2020-09-22 16:57:52 +0100

    Ticket 1 minimum required amount finished

--- NewSoloProject/solo.py
+++ NewSoloProject/solo.py
@@ -1,14 +1,32 @@
 import flask, uuid
 from flask import request, render_template, flash, url_for, redirect, abort, Response
-from forms import PassForm
 
-#---------------------------------------------------------#
+
+#---------------------------------------------------------
+
+class Member:
+
+    Memberlist = []
+
+    def __init__(self, firstname, lastname, role):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = firstname + "." + lastname + "@email.com"
+        self.role = role
+        self.fulldetails = "Name: " + firstname + " " + lastname + " " + "Email: " +  self.email + " " + "Role: " + role
+        Member.Memberlist.append(self.fulldetails) 
+        
+dev1 = Member("John", "Marston", "manager")
+dev2 = Member("Arthur", "Morgan", "Programmer")
+
+#---------------------------------------------------------
 
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('main.html')
+    teamlist = Member.Memberlist
+    return render_template('main.html', teamlist = teamlist)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,4 +37,5 @@ if __name__ == '__main__':
     #app.config.from_object(Config)
     app.run(host="0.0.0.0", debug=True)
 
-#----------------------------------------------------------#
+
+
